643f8fb0adb72359a968ab80416ba5e31643f8d9
bomii1 <buij304@naver.com>
2023-05-07 18:44:45 +0900

    소수찾기-에라토스테네스의 체

---
+++ 0507.py
@@ -0,0 +1,24 @@
+import math
+#에라토스테네스의 체
+def solution(n):
+    cnt = 0
+    for i in range(2, n+1):
+        if isPrime(i):
+            cnt += 1
+    return cnt
+def isPrime(n):
+    for i in range(2, int(math.sqrt(n))+1):
+        if n % i == 0:
+            return False
+    return True
+
+"""
+def solution(n):
+    num=set(range(2,n+1))
+
+    for i in range(2,int(math.sqrt(n))+1):
+        if i in num:
+            num-=set(range(2*i,n+1,i))
+    return len(num)
+print(solution(10))
+"""
